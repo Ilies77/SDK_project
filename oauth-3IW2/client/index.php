4750f704a1abb7c0dc8d78eb859732a6bde24493
Iliès <islim1@myges.fr>
2022-07-05 17:16:28 +0200

    push v1 Facebook

--- oauth-3IW2/client/index.php
+++ oauth-3IW2/client/index.php
@@ -1,16 +1,9 @@
 <?php
 
 define("CLIENT_ID", '67dc2be521bec2ff862d3ab057de216b');
+define("FB_CLIENT_ID", '1311135729390173');
 define("CLIENT_SECRET", '04054cf433eeb3976252c81b6d657fda');
-define("FB_CLIENT_ID", '439698237646679');
-define("FB_CLIENT_SECRET", 'f388515768af186b09562a9897c2605d');
-
-define("GOOGLE_CLIENT_ID", '717148236814-gnf3acamnmn8dhqmlsj2nfmpqfuqmhcr.apps.googleusercontent.com');
-define("GOOGLE_CLIENT_SECRET", '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');
-
-define("TWITCH_CLIENT_ID", 'tsy0kkksrmh9ijkhlg8ledyadl39pp');
-define("TWITCH_CLIENT_SECRET", 'h1f0h1mmm3tbc9pswh23z7zb57b77l');
-
+define("FB_CLIENT_SECRET", 'fc5e25661fe961ab85d130779357541e');
 
 // Create a login page with a link to oauth
 function login()
@@ -35,19 +28,8 @@ function login()
         "scope"=>"public_profile,email",
         "redirect_uri"=>"https://localhost/fb_oauth_success",
     ]);
-    $TwitchQueryParams = http_build_query([
-        "state"=>bin2hex(random_bytes(16)),
-        "response_type"=>"token",
-        "client_id"=> TWITCH_CLIENT_ID,
-        "redirect_uri"=>"https://localhost/twitch_oauth_success",
-        "scope"=>"public_profile",
-        
-    ]);
     echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a><br>";
-    echo "<a href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a><br>";
-    echo "<a href=\"https://id.twitch.tv/oauth2/authorize?{$TwitchQueryParams}\">Login with Twitch</a>";
-    
-   
+    echo "<a href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a>";
 }
 
 // get token from code then get user info
@@ -94,55 +76,6 @@ function callback()
         return;
     }
     var_dump(json_decode($response, true));
-}
-
-function twittercallback()
-{
-    $token = getTToken("https://graph.twitter.com/v13.0/oauth/access_token", TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET);
-    $user = getTwitterUser($token);
-    $unifiedUser = (fn () => [
-        "id" => $user["id"],
-        "name" => $user["name"],
-        "email" => $user["email"],
-        "firstName" => $user['first_name'],
-        "lastName" => $user['last_name'],
-    ])();
-    var_dump($unifiedUser);
-}
-function getTwitterUser($token)
-{
-    $context = stream_context_create([
-        "http"=>[
-            "header"=>"Authorization: Bearer {$token}"
-        ]
-    ]);
-    $response = file_get_contents("https://graph.twitter.com/v13.0/me?fields=last_name,first_name,email", false, $context);
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    return json_decode($response, true);
-}
-function getTToken($baseUrl, $clientId, $clientSecret)
-{
-    ["code"=> $code, "state" => $state] = $_GET;
-    $queryParams = http_build_query([
-        "client_id"=> $clientId,
-        "client_secret"=> $clientSecret,
-        "redirect_uri"=>"https://localhost/twitter_oauth_success",
-        "code"=> $code,
-        "grant_type"=>"authorization_code",
-    ]);
-
-    $url = $baseUrl . "?{$queryParams}";
-    $response = file_get_contents($url);
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    ["access_token" => $token] = json_decode($response, true);
-
-    return $token;
 }
 
 // Facebook oauth: exchange code with token then get user info
@@ -205,9 +138,6 @@ switch (strtok($route, "?")) {
         break;
     case '/fb_oauth_success':
         fbcallback();
-        break;
-    case '/twitch_oauth_success':
-        twitchcallback();
         break;
     default:
         http_response_code(404);
